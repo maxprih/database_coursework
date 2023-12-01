376ca1350053cf740cdddd3aaa7c595aeb540432
maxpri <max_pri@niuitmo.ru>
2023-12-01 19:38:18 +0300

    updated db

--- src/main/resources/course3/triggers.sql
+++ src/main/resources/course3/triggers.sql
@@ -1,11 +1,11 @@
-DROP TRIGGER IF EXISTS check_match_date_within_league_date_trigger ON match;
+-- DROP TRIGGER IF EXISTS check_match_date_within_league_date_trigger ON match;
 CREATE TRIGGER check_match_date_within_league_date_trigger
     BEFORE INSERT
     ON Match
     FOR EACH ROW
 EXECUTE FUNCTION check_match_date_within_league_date();
 
-DROP TRIGGER IF EXISTS update_bet_status_trigger ON match_event;
+-- DROP TRIGGER IF EXISTS update_bet_status_trigger ON match_event;
 CREATE TRIGGER update_bet_status_trigger
     AFTER UPDATE
     ON Match_Event
@@ -13,14 +13,14 @@ CREATE TRIGGER update_bet_status_trigger
     WHEN (OLD.status IS DISTINCT FROM NEW.status)
 EXECUTE FUNCTION update_bet_status();
 
-DROP TRIGGER IF EXISTS before_insert_bet ON bet;
+-- DROP TRIGGER IF EXISTS before_insert_bet ON bet;
 CREATE TRIGGER before_insert_bet
     BEFORE INSERT
     ON Bet
     FOR EACH ROW
 EXECUTE FUNCTION check_bet_amount();
 
-DROP TRIGGER IF EXISTS after_update_bet_status ON bet;
+-- DROP TRIGGER IF EXISTS after_update_bet_status ON bet;
 CREATE TRIGGER after_update_bet_status
     AFTER UPDATE
     ON Bet
